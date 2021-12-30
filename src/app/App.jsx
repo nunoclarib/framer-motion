23b9966273ce8171ca347b96f468c867c5aac027
Nuno Ribeiro <nunocpr21@gmail.com>
2021-12-30 14:55:20 +0000

    feat: seatup

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {motion} from 'framer-motion'
+
+
+const App = () => {
+
+  return (
+    <motion.div>
+      <h1>Hello World!</h1>
+    </motion.div>
+  );
+}
+export default App;
